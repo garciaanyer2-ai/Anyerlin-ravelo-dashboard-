ae6752b886a8cb59b7cd4811d1e1bd5752ec7da6
garciaanyer2-ai <garciaanyer2@gmail.com>
2025-12-09 18:23:00 +0100

    Add conclusion to taxi.py about learning experience
    conclusión

---
+++ taxi.py
@@ -0,0 +1,7 @@
+En conclusión, con el ejercicio de días de elaboración, que realicé
+utilizando Google Colab (Colaboratory) debido a la falta de una computadora,
+he comprendido y puesto en práctica habilidades cruciales. Ha sido impresionante
+ver cómo, a partir de una serie de números que inicialmente parecían 'sin sentido',
+podemos descubrir todo un mundo de información y patrones detrás de ellos. Python, 
+incluso ejecutado en la nube, es una increíble herramienta para transformar datos y
+abrir todo un mundo al entorno digital. y estoy con ancias de seguir aprendiendo mas.
